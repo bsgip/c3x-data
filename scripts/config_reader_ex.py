672f70cfe5565f8117a6b89c0af4d8577f6ba3b1
Meike Moeckel <meike.moeckel@anu.edu.au>
2020-11-03 15:49:51 +1100

    added new function for config reading to example and created a full config file for testing purposes

--- scripts/config_reader_ex.py
+++ scripts/config_reader_ex.py
@@ -5,7 +5,7 @@
 from c3x.data_loaders import configfileparser
 
 # Reads a config file to produce a dictionary that can be handed over to functions
-config = configfileparser.ConfigFileParser("config/example_for_cleaning.ini")
+config = configfileparser.ConfigFileParser("config/example_full_config.ini")
 
 data_paths = config.read_data_path()
 batch_info = config.read_batches()
@@ -16,3 +16,10 @@ data_usage = config.read_data_usage()
 nan_handling = config.read_nan_handeling()
 resampling = config.read_resampling()
 refill = config.read_refill()
+optimiser_objectives_set = config.read_optimiser_objective_set()
+optimiser_objectives = config.read_optimiser_objectives()
+inverter = config.read_inverter()
+energy_storage = config.read_energy_storage()
+energy_system = config.read_energy_system()
+tariff_factors = config.read_tariff_factors()
+scenario = config.read_scenario_info()
